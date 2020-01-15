583271cf730739be0914cb5ea216f1cf4815fb6c
multicampus <pypy7000@korea.ac.kr>
2020-01-15 15:52:56 +0900

    add frame

---
+++ 5zo/src/components/app_name/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class index extends React.Component{
+    render(){
+        return <div>첫화면</div>
+    }
+}
+
+
+export default index;
---
+++ 5zo/src/components/app_name/second.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const second = () => {
+  return <div>두번째 화면</div>;
+};
+
+export default second;
