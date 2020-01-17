3b54c31e1e5bd4b95bd56f52afdb48f98632f6b4
multicampus <pypy7000@korea.ac.kr>
2020-01-17 09:44:21 +0900

    add data

--- 5zo/src/actions/index.js
+++ 5zo/src/actions/index.js
@@ -4,5 +4,5 @@ import { FETCH_MEMBERS } from "./types";
 
 export const fetchMembers = () => async dispatch => {
   const response = await apis.get("/member");
-  dispatch({ type: FETCH_MEMBERS, payload: response.data });
+  dispatch({ type: FETCH_MEMBERS, payload: response.data.data });
 };
