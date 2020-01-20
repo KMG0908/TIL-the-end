f23528ddd4a8110f18c9081ca7f374ff45d7dcf7
yuninje <dbsdlswp@naver.com>
2020-01-20 09:10:14 +0900

    태그 클라우드 페이지

--- 5zo/src/components/serach/Search.js
+++ 5zo/src/components/serach/Search.js
@@ -1,16 +1,36 @@
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 import queryString from "query-string";
 
-class Search extends React.Component {
-  
-  render() {
-    const query = queryString.parse(this.props.location.search);
-    const user_id = this.props.match.params.user_id
-    console.log(user_id) /*user_id 받아오기 */
-    console.log(query.q) /*q 받아오기 */
 
-  return <div>{user_id}님의 TIL #{query.q}</div>;
-  }
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: 200,
+    },
+  },
+}));
+
+const searchBar = () => {
+  return <TextField id="standard-basic" label="Standard" />
 }
 
-export default Search;
+export default function search(props){
+  const classes = useStyles();
+  
+  const query = queryString.parse(props.location.search);
+  const user_id = props.match.params.user_id
+  console.log(user_id) /*user_id 받아오기 */
+  console.log(query.q) /*q 받아오기 */
+
+  return (
+    <div>
+      <div>{user_id}님의 TIL #{query.q}</div>
+      <form className={classes.root} >
+        {searchBar()}
+      </form>
+    </div>
+  )
+}
--- 5zo/src/components/tags/Tags.js
+++ 5zo/src/components/tags/Tags.js
@@ -1,9 +1,47 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
-class Tags extends React.Component {
-  render() {
-    return <div>해쉬태그예정</div>;
-  }
+
+const tags = [
+  { id: 1, name: 'Node.js' },
+  { id: 2, name: 'React' },
+  { id: 3, name: 'Java' },
+  { id: 4, name: '어려어' },
+  { id: 5, name: '미췬' },
+  { id: 6, name: '의식의 흐름대로' },
+  { id: 7, name: '우오오오오' },
+]
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+const tagButtons = () => tags.map((tag) =>
+
+  <Link to={'/searchTag/'+tag.name} key={'link'+tag.id}>
+    <Button
+      variant="contained"
+      key={tag.id}>
+      {tag.name}
+    </Button>
+  </Link>
+);
+
+export default function ContainedButtons() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      해쉬태그예정
+      {tagButtons()}
+    </div>
+  );
 }
 
-export default Tags;
+
