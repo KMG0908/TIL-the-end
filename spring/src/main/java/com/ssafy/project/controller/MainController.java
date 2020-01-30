51b2de4a6c13ac39e2261451b33923d198c6bc10
권순국 <refresh6724@soongsil.ac.kr>
2020-01-30 16:27:22 +0900

    0.1.5 => 0.1.6
    https://joshua-dev-story.blogspot.com/2020/01/react-spring-2.html

--- spring/src/main/java/com/ssafy/project/controller/MainController.java
+++ spring/src/main/java/com/ssafy/project/controller/MainController.java
@@ -3,13 +3,13 @@ package com.ssafy.project.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.ssafy.project.service.MemberService;
 
 @Controller
 public class MainController {
+	
 	@Autowired
 	MemberService service;
 
