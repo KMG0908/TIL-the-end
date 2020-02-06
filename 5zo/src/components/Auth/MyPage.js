33819fc2572bc655365ad04d255cfca8dcc65206
yuninje <dbsdlswp@naver.com>
2020-02-06 16:28:02 +0900

    member 페이지 UI 수정

--- 5zo/src/components/Auth/MyPage.js
+++ 5zo/src/components/Auth/MyPage.js
@@ -11,10 +11,10 @@ class MyPage extends Component {
     return (
       <div style={{textAlign : 'center'}}>
       <div style={{display : 'inline-block', minWidth : 500}}>
-          <h1> 내 정보 </h1>
-        <AuthWrapper>
+        <AuthWrapper title="내 정보">
           <AuthContent>
               {/* 로그인한 member 의 데이터를 아래 TextWithLabel 들의 value 에 줘야함. */}
+            <TextWithLabel label="아이디" name="ID"  value={this.props.mem_info ? this.props.mem_info.mem_id : ''} />
             <TextWithLabel label="이메일" name="email"  value={this.props.mem_info ? this.props.mem_info.mem_email : ''} type="email"/>
             <TextWithLabel label="닉네임" name="nick" value={this.props.mem_info ? this.props.mem_info.mem_nick:''} />
             <RightAlignedLink to="/mypage/edit"> 수정 </RightAlignedLink>
--- 5zo/src/components/Auth/Register.js
+++ 5zo/src/components/Auth/Register.js
@@ -42,39 +42,44 @@ class Register extends Component {
         break;
     }
   }
-  register(){
+  register() {
     this.props.register(this.state.loginId, this.state.loginPw, this.state.email, this.state.nick)
   }
-  cancelRegister(){
+  cancelRegister() {
     window.location.href = "/"
   }
   render() {
-    if(this.props.register_id && this.props.register_id !== ""){
+    if (this.props.register_id && this.props.register_id !== "") {
       this.props.registerReset();
       alert('회원가입 완료')
-      window.location.href='/login'
+      window.location.href = '/login'
     }
     return (
-      <AuthWrapper>
-        <AuthContent title="회원가입" id="register">
-          <InputWithLabel label="아이디" id="loginId" name="loginId" placeholder="아이디( 5 ~ 20 자 )" onChange={this.handleChange} maxLength="20"/>
-          <InputWithLabel label="비밀번호" id="loginPw" name="loginPw" placeholder="비밀번호 ( 8 ~ 16 자 )" type="password" onChange={this.handleChange} maxLength="16"/>
-          <InputWithLabel label="이메일" id="email" name="email" placeholder="이메일" type="email" onChange={this.handleChange}/>
-          <InputWithLabel label="닉네임" id="nick" name="nick" placeholder="닉네임" onChange={this.handleChange}/>
-          {this.props.register_err}
-          <AuthButton onClick={this.register}> 회원가입 </AuthButton>
 
-          {this.props.location.pathname === "/" ? null :<AuthButton onClick={this.cancelRegister}> 취소 </AuthButton>}
-        </AuthContent>
-      </AuthWrapper>
+      <div style={{ textAlign: 'center' }}>
+        <div style={{ display: 'inline-block', minWidth: 500 }}>
+        <AuthWrapper>
+          <AuthContent title="회원가입" id="register">
+            <InputWithLabel label="아이디" id="loginId" name="loginId" placeholder="아이디( 5 ~ 20 자 )" onChange={this.handleChange} maxLength="20" />
+            <InputWithLabel label="비밀번호" id="loginPw" name="loginPw" placeholder="비밀번호 ( 8 ~ 16 자 )" type="password" onChange={this.handleChange} maxLength="16" />
+            <InputWithLabel label="이메일" id="email" name="email" placeholder="이메일" type="email" onChange={this.handleChange} />
+            <InputWithLabel label="닉네임" id="nick" name="nick" placeholder="닉네임" onChange={this.handleChange} />
+            {this.props.register_err}
+            <AuthButton onClick={this.register}> 회원가입 </AuthButton>
+
+            {this.props.location.pathname === "/" ? null : <AuthButton onClick={this.cancelRegister}> 취소 </AuthButton>}
+          </AuthContent>
+        </AuthWrapper>
+      </div>
+      </div>
     );
   }
 }
 
 const mapStatetoProps = state => {
   return {
-    register_id : state.members.register_id,
-    register_err : state.members.register_err
+    register_id: state.members.register_id,
+    register_err: state.members.register_err
   };
 };
 
