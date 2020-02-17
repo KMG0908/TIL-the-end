51b4d9e857d3dbabdc5398e42dc7a829d645fa37
권순국 <refresh6724@soongsil.ac.kr>
2020-02-17 11:40:55 +0900

    0.2.6 hotfix ss.SSS to ss

--- spring/src/main/java/com/ssafy/project/dto/Alarm.java
+++ spring/src/main/java/com/ssafy/project/dto/Alarm.java
@@ -14,7 +14,7 @@ public class Alarm implements Serializable {
 	private int alarm_id;
 	private String mem_id;
 	private String alarm_text;
-	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss.SSS")
+	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
 	private Timestamp alarm_date;
 	private String alarm_url;
 
