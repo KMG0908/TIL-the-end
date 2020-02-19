7a45b51b9ceb8359356100c0cb3c829eb335013c
권순국 <refresh6724@soongsil.ac.kr>
2020-02-19 17:00:13 +0900

    0.2.11 config / timezone asia seoul

--- spring/src/main/java/com/ssafy/project/dto/Alarm.java
+++ spring/src/main/java/com/ssafy/project/dto/Alarm.java
@@ -1,7 +1,7 @@
 package com.ssafy.project.dto;
 
 import java.io.Serializable;
-import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
@@ -14,8 +14,8 @@ public class Alarm implements Serializable {
 	private int alarm_id;
 	private String mem_id;
 	private String alarm_text;
-	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
-	private Timestamp alarm_date;
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss") //, timezone = "Asia/Seoul"
+	private LocalDateTime alarm_date;
 	private String alarm_url;
 
 }
