749e0bf82d1c43c06669b1c700c327e85d201171
multicampus <pypy7000@korea.ac.kr>
2020-02-20 14:37:46 +0900

    add comment onblur

--- 5zo/src/components/daily/comment/CommentForm.js
+++ 5zo/src/components/daily/comment/CommentForm.js
@@ -2,7 +2,7 @@ import React from "react";
 import Input from "@material-ui/core/Input";
 import { addComment } from "../../../actions";
 import { connect } from "react-redux";
-import SubdirectoryArrowRightIcon from '@material-ui/icons/SubdirectoryArrowRight';
+import SubdirectoryArrowRightIcon from "@material-ui/icons/SubdirectoryArrowRight";
 import Divider from "@material-ui/core/Divider";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItem from "@material-ui/core/ListItem";
@@ -10,12 +10,11 @@ import Button from "@material-ui/core/Button";
 
 function CommentForm(props) {
   const [comment_contents, setCommentContents] = React.useState(
-    props.comment_contents ? 
-      props.comment_contents : 
-      ( props.mem_id ?
-          `@${props.mem_id} ` :
-          ""
-      )
+    props.comment_contents
+      ? props.comment_contents
+      : props.mem_id
+      ? `@${props.mem_id} `
+      : ""
   );
 
   const handleSubmit = event => {
@@ -26,23 +25,31 @@ function CommentForm(props) {
   };
 
   const handleChange = event => {
-    const value = event.target.value
-    const originValue = props.mem_id ? `@${props.mem_id} ` : ""
-    if(value.length < originValue.length){
-      setCommentContents(originValue)
-    }else{
-      setCommentContents(value)
+    const value = event.target.value;
+    const originValue = props.mem_id ? `@${props.mem_id} ` : "";
+    if (value.length < originValue.length) {
+      setCommentContents(originValue);
+    } else {
+      setCommentContents(value);
     }
-  }
+  };
 
   return (
-    <div className={props.inputRef? "write_reply" : ""}>
+    <div className={props.inputRef ? "write_reply" : ""}>
       <ListItem>
-        <ListItemIcon style={{display: props.inputRef? "flex" : "none"}}>
-          <SubdirectoryArrowRightIcon style={{color: "rgba(0, 0, 0, 0.3)"}} />
+        <ListItemIcon style={{ display: props.inputRef ? "flex" : "none" }}>
+          <SubdirectoryArrowRightIcon style={{ color: "rgba(0, 0, 0, 0.3)" }} />
         </ListItemIcon>
-        <div className={props.inputRef? "reply_window" : "comment_window"}>
-          <form style={{ width: "100%", height: "100%", marginRight: '10px', display: 'flex', alignItems: 'center'}}>
+        <div className={props.inputRef ? "reply_window" : "comment_window"}>
+          <form
+            style={{
+              width: "100%",
+              height: "100%",
+              marginRight: "10px",
+              display: "flex",
+              alignItems: "center"
+            }}
+          >
             <Input
               inputRef={props.inputRef}
               style={{ width: "93%", height: "100%", wordBreak: "break-all" }}
@@ -51,16 +58,22 @@ function CommentForm(props) {
               value={comment_contents}
               disableUnderline
               multiline
-              autoFocus = {true}
-              inputProps={{maxLength:200}}
-              onBlur = {props.onBlur}
-              onFocus = { e=> {
-                const value = e.target.value
-                e.target.value = ""
-                e.target.value = value
+              autoFocus={true}
+              inputProps={{ maxLength: 200 }}
+              onBlur={props.onBlur}
+              onFocus={e => {
+                const value = e.target.value;
+                e.target.value = "";
+                e.target.value = value;
               }}
             />
-            <Button variant="outlined" className="comment_register" onClick={handleSubmit}>등록</Button>
+            <Button
+              variant="outlined"
+              className="comment_register"
+              onMouseDown={handleSubmit}
+            >
+              등록
+            </Button>
           </form>
         </div>
       </ListItem>
